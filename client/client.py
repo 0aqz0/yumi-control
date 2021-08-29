4ff7e71441d4f67ad11ff24d59a141e843dfb7d4
0aqz0 <aqz@zju.edu.cn>
2021-08-29 20:22:02 +0800

    add inspire hand control

--- client/client.py
+++ client/client.py
@@ -3,11 +3,16 @@ import time
 import egm_pb2
 import h5py
 import numpy as np
+from inspire_hand import InspireHand
 
 # IP address & ports
 CLIENT_IP = "192.168.125.10"
 CLIENT_PORT_L = 6510
 CLIENT_PORT_R = 6511
+
+# inspire hand port name
+left_hand_port = 'COM12'
+right_hand_port = 'COM13'
 
 # h5 file path
 H5_FILE_PATH = '../../yumi-ubuntu-backup/sign_robot_control/h5_data/experiment/2021-3-21-processed/processed-wo.h5'
@@ -86,6 +91,40 @@ def parse_data(data):
         print("No header")
     return joints
 
+"""
+convert glove angles to electrical signal
+"""
+def convert_glove_angles(glove_angles):
+    # Input *glove_angles* is of the size (50, 12)
+    ndata = glove_angles.shape[0]
+    elec_signal = np.zeros((ndata, 6), dtype="int32")
+    optim_fourfin_range = [0, -1.6]
+    optim_thumbroll_range = [0.1, 0.0]
+    optim_thumbrot_range = [-1.0, 0.3] #[0.3, -1.0] # actually fixed at intermediate position when we still use S14 glove, since it doesn't measure this axis of motion
+    max_elec = 2000
+    min_elec = 0
+
+    # conversion
+    for i in range(ndata):
+        # four fingers (for inspire hand electrical signal, order is: pinky->ring->middle->index)
+        elec_signal[i, 3] = round((glove_angles[i, 0] - optim_fourfin_range[0]) / (optim_fourfin_range[1] - optim_fourfin_range[0]) * (max_elec - min_elec))
+        elec_signal[i, 2] = round((glove_angles[i, 2] - optim_fourfin_range[0]) / (optim_fourfin_range[1] - optim_fourfin_range[0]) * (max_elec - min_elec))
+        elec_signal[i, 1] = round((glove_angles[i, 4] - optim_fourfin_range[0]) / (optim_fourfin_range[1] - optim_fourfin_range[0]) * (max_elec - min_elec))
+        elec_signal[i, 0] = round((glove_angles[i, 6] - optim_fourfin_range[0]) / (optim_fourfin_range[1] - optim_fourfin_range[0]) * (max_elec - min_elec))
+        # thumb roll
+        elec_signal[i, 4] = round((glove_angles[i, 9] - optim_thumbroll_range[0]) / (optim_thumbroll_range[1] - optim_thumbroll_range[0]) * (max_elec - min_elec))
+        # thumb rot (actually fixed when using S14 generated data)
+        elec_signal[i, 5] = round((glove_angles[i, 8] - optim_thumbrot_range[0]) / (optim_thumbrot_range[1] - optim_thumbrot_range[0]) * (max_elec - min_elec))
+
+        # check if within range
+        for j in range(6):
+            if elec_signal[i, j] < 0 or elec_signal[i, j] > 2000:
+                print("Error: Joint angle J{}={} of path point {} is out of bounds!".format(i, glove_angles[i, j], j))
+                return None
+
+    return elec_signal
+
+
 if __name__ == '__main__':
     # socket for left arm
     sock_L = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -94,14 +133,23 @@ if __name__ == '__main__':
     sock_R = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock_R.bind((CLIENT_IP, CLIENT_PORT_R))
 
+    # inspire hands
+    left_hand_controller = InspireHand(left_hand_port, 115200)
+    right_hand_controller = InspireHand(right_hand_port, 115200)
+    left_hand_controller.setspeed(1000, 1000, 1000, 1000, 1000, 1000)
+    right_hand_controller.setspeed(1000, 1000, 1000, 1000, 1000, 1000)
+
     # parse h5 file
     l_joint_angles, r_joint_angles, l_glove_angles, r_glove_angles = parse_h5(H5_FILE_PATH)
     l_joint_angles = switch_joint_angle(rad2degree(clamp_to_limits(l_joint_angles, yumi_left_lower_limits, yumi_left_upper_limits)))
     r_joint_angles = switch_joint_angle(rad2degree(clamp_to_limits(r_joint_angles, yumi_right_lower_limits, yumi_right_upper_limits)))
+    l_glove_signal = convert_glove_angles(l_glove_angles)
+    r_glove_signal = convert_glove_angles(r_glove_angles)
     total_frames = len(l_joint_angles)
     t = 0
     done = False
-    last_command_time = -float('Inf')
+    last_arm_command_time = -float('Inf')
+    last_hand_command_time = -float('Inf')
     last_verbose_time = -float('Inf')
 
     while True:
@@ -131,11 +179,17 @@ if __name__ == '__main__':
             if t == total_frames:
                 t = 0
         # send control command not too fast
-        elif time.time() - last_command_time > 0.005:
+        elif time.time() - last_arm_command_time > 0.005:
             # left arm
             pSensorMessage = createSensorMsg(l_joint_angles[t])
             sock_L.sendto(pSensorMessage.SerializeToString(), server_L)
             # right arm
             pSensorMessage = createSensorMsg(r_joint_angles[t])
             sock_R.sendto(pSensorMessage.SerializeToString(), server_R)
-            last_command_time = time.time()
+            last_arm_command_time = time.time()
+
+        # set inspire hand pos
+        if time.time() - last_hand_command_time > 0.005:
+            left_hand_controller.setpos(l_glove_signal[t, 0], l_glove_signal[t, 1], l_glove_signal[t, 2], l_glove_signal[t, 3], l_glove_signal[t, 4], l_glove_signal[t, 5])
+            right_hand_controller.setpos(r_glove_signal[t, 0], r_glove_signal[t, 1], r_glove_signal[t, 2], r_glove_signal[t, 3], r_glove_signal[t, 4], r_glove_signal[t, 5])
+            last_hand_command_time = time.time()
